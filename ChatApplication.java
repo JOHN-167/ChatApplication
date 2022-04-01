fc3b8356b2af12ef3f3fbed9edb54216ca190ed0
JOHN-167 <91348778+JOHN-167@users.noreply.github.com>
2022-03-31 22:14:28 -0400

    Add files via upload

--- ChatApplication.java
+++ ChatApplication.java
@@ -4,6 +4,7 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.Timer;
+import java.io.*;
 
 public class ChatApplication extends JFrame {
     //constructor
@@ -11,17 +12,31 @@ public class ChatApplication extends JFrame {
         setSize(600,480);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setFocusable(true);
+        setLayout(new FlowLayout());
 
         //chat gui
-        JPanel loginComponent = new JPanel();
-        // loginComponent.setBackground(Color.WHITE);
+        JPanel bgPanel = new JPanel();
+        bgPanel.setBackground(Color.PINK);
+        add(bgPanel);
+
+        /*
+        JComponent loginComponent = new JComponent(){
+            @Override
+            public void paintComponent(Graphics g) {}
+        };
+        JComponent placeholder = new JComponent(){
+            @Override
+            public void paintComponent(Graphics g) {}
+        };
+        placeholder.setMaximumSize(new Dimension(200,150));
+        bgPanel.add(placeholder);
         loginComponent.setFocusable(true);
         loginComponent.setLayout(new BoxLayout(loginComponent, BoxLayout.Y_AXIS));
         JLabel userLabel = new JLabel("Username");
         userLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
         loginComponent.add(userLabel);
         JTextField username = new JTextField();
-        username.setMaximumSize(new Dimension(100,30));
+        username.setMaximumSize(new Dimension(200,30));
         username.setAlignmentX(Component.CENTER_ALIGNMENT);
         username.setVisible(true);
         loginComponent.add(username);
@@ -29,18 +44,68 @@ public class ChatApplication extends JFrame {
         passLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
         loginComponent.add(passLabel);
         JTextField password = new JTextField();
-        password.setMaximumSize(new Dimension(100,30));
+        password.setMaximumSize(new Dimension(200,30));
         password.setAlignmentX(Component.CENTER_ALIGNMENT);
         password.setVisible(true);
         loginComponent.add(password);
-        JPanel loginPage = new JPanel();
-        loginPage.setBackground(Color.PINK);
-        loginPage.setLayout(new BoxLayout(loginPage, BoxLayout.PAGE_AXIS));
-        // loginComponent.setAlignmentX(Component.CENTER_ALIGNMENT);
-        loginComponent.setAlignmentY(Component.CENTER_ALIGNMENT);
-        // loginComponent.setPreferredSize(new Dimension(200,150));
-        loginPage.add(loginComponent);
-        add(loginPage);
+        bgPanel.setLayout(new BoxLayout(bgPanel, BoxLayout.PAGE_AXIS));
+        // loginComponent.setAlignmentY(Component.CENTER_ALIGNMENT);
+        loginComponent.setPreferredSize(new Dimension(200,150));
+        JButton loginButton = new JButton("Log in");
+        loginButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent a){
+                String user = "", pass = "";
+                Boolean successful = false;
+                try {
+                    FileReader file = new FileReader("loginDetails.txt");
+                    Scanner scan = new Scanner(file);
+                    while (scan.hasNextLine() && successful == false){
+                        String input = scan.nextLine(); 
+                        Scanner sc = new Scanner(input);
+                        sc.useDelimiter("\t");
+                        user = sc.next();
+                        pass = sc.next();
+                        System.out.printf("User = %s, Pass = %s%n",user,pass);
+                        if (user.equals(username.getText()) &&
+                            pass.equals(password.getText())) {
+                            successful = true;
+                        }
+                        sc.close();
+                    }
+                    scan.close();
+                } catch (FileNotFoundException e){
+                    System.out.println(e);
+                    System.exit(0);
+                } catch (Exception e) {
+                    System.out.println(e);
+                    System.out.println("NoMatch");
+                }
+                if (successful == true){
+                    bgPanel.remove(loginComponent);
+                    bgPanel.revalidate();
+                    bgPanel.repaint();
+                } else {
+                    username.setText("");
+                    password.setText("");
+                }
+            }
+        }); 
+        loginButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        loginComponent.add(loginButton);
+        bgPanel.add(loginComponent);
+        */
+    
+        bgPanel.setPreferredSize(new Dimension(getWidth(),getHeight()-80));
+        JTextField chatField = new JTextField();
+        JButton sendButton = new JButton("Send");
+        chatField.setColumns(40);
+        JPanel chatPanel = new JPanel();
+        chatPanel.setBackground(Color.WHITE);
+        chatPanel.add(chatField);
+        chatPanel.add(sendButton);
+        chatPanel.setPreferredSize(new Dimension(getWidth(),80));
+        add(chatPanel);
     }
     public static void main(String[] args){
         new ChatApplication().setVisible(true);
